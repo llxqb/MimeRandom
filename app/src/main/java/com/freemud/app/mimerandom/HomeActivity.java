15b340f156d53e7e7ddc44a06b4a1e8d58b0c134
llxqb <llxqb_tt@126.com>
2018-09-14 16:49:03 +0800

    增加home

--- app/src/main/java/com/freemud/app/mimerandom/HomeActivity.java
+++ app/src/main/java/com/freemud/app/mimerandom/HomeActivity.java
@@ -11,21 +11,26 @@ import java.util.Random;
 
 public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private TextView mHomeRandomValue;
+    private TextView mHomeRandomValue, mHomeRandomValue2,mHomeRandomBossValue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-
         initView();
     }
 
     private void initView() {
         Button mHomeRandom = findViewById(R.id.home_random);
         mHomeRandomValue = findViewById(R.id.home_random_value);
+        Button mHomeRandom2 = findViewById(R.id.home_random2);
+        mHomeRandomValue2 = findViewById(R.id.home_random_value2);
+        Button mHomeRandomBoss = findViewById(R.id.home_random_boss);
+        mHomeRandomBossValue = findViewById(R.id.home_random_boss_value);
         Button mHomeGoMain = findViewById(R.id.home_go_main);
         mHomeRandom.setOnClickListener(this);
+        mHomeRandom2.setOnClickListener(this);
+        mHomeRandomBoss.setOnClickListener(this);
         mHomeGoMain.setOnClickListener(this);
     }
 
@@ -33,7 +38,13 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.home_random:
-                getMyRandom();
+                getMyRandom(1);
+                break;
+            case R.id.home_random2:
+                getMyRandom(2);
+                break;
+            case R.id.home_random_boss:
+                getMyRandom(3);
                 break;
             case R.id.home_go_main:
                 MainActivity.start(this);
@@ -42,18 +53,42 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
     }
 
 
-    private void getMyRandom() {
+    private void getMyRandom(int type) {
         Random random = new Random();
         int count = random.nextInt(4);
-        Toast.makeText(this,"count:"+count,Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "count:" + count, Toast.LENGTH_SHORT).show();
         if (count == 0) {
-            mHomeRandomValue.setText("中奖：大");
+            if (type == 1) {
+                mHomeRandomValue.setText("大");
+            } else if (type == 2) {
+                mHomeRandomValue2.setText("大");
+            }else if(type ==3){
+                mHomeRandomBossValue.setText("绝杀：大");
+            }
         } else if (count == 1) {
-            mHomeRandomValue.setText("中奖：小");
+            if (type == 1) {
+                mHomeRandomValue.setText("小");
+            } else if (type == 2) {
+                mHomeRandomValue2.setText("小");
+            }else if(type ==3){
+                mHomeRandomBossValue.setText("绝杀：小");
+            }
         } else if (count == 2) {
-            mHomeRandomValue.setText("中奖：单");
+            if (type == 1) {
+                mHomeRandomValue.setText("单");
+            } else if (type == 2) {
+                mHomeRandomValue2.setText("单");
+            }else if(type ==3){
+                mHomeRandomBossValue.setText("绝杀：单");
+            }
         } else if (count == 3) {
-            mHomeRandomValue.setText("中奖：双");
+            if (type == 1) {
+                mHomeRandomValue.setText("双");
+            } else if (type == 2) {
+                mHomeRandomValue2.setText("双");
+            }else if(type ==3){
+                mHomeRandomBossValue.setText("绝杀：双");
+            }
         }
     }
 }
